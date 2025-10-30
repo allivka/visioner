37a255689ef9c9fc8834288e3945059eb0f070d8
allivka <allivka1920@gmail.com>
2025-10-30 23:41:43 +0300

    tested it on my platform and it works, kind of. Motors seem to work really weird, the sometimes do not start spiining and sometimes do not stop spinning, it is really strange, have no ideas waht to do with, maybe i will ned to have couple of tests with lower level platform control"

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,29 @@ double wheelR = 0.1;
 double motorDistance = 0.3;
 
 platform::PlatformMotorConfig config({
-    motor::MotorInfo(-90, motorDistance, wheelR, motorUseSpeedRange, motorInterfaceAngularSpeedRange),
-    motor::MotorInfo(0, motorDistance, wheelR, motorUseSpeedRange, motorInterfaceAngularSpeedRange),
     motor::MotorInfo(90, motorDistance, wheelR, motorUseSpeedRange, motorInterfaceAngularSpeedRange),
-    motor::MotorInfo(180, motorDistance, wheelR, motorUseSpeedRange, motorInterfaceAngularSpeedRange)
+    motor::MotorInfo(180, motorDistance, wheelR, motorUseSpeedRange, motorInterfaceAngularSpeedRange),
+    motor::MotorInfo(-90, motorDistance, wheelR, motorUseSpeedRange, motorInterfaceAngularSpeedRange),
+    motor::MotorInfo(0, motorDistance, wheelR, motorUseSpeedRange, motorInterfaceAngularSpeedRange)
 });
 
 platform::Platform<V5::motor::V5MotorController> plat(config);
 
 void setup() {  
     Vex5.begin();
-    plat.init(util::Array<VEX5_PORT_t>({(VEX5_PORT_t)1, (VEX5_PORT_t)2, (VEX5_PORT_t)3, (VEX5_PORT_t)4}));
     Serial.begin(9600);
-    // delay(5000);
+    
+    auto e = plat.init(util::Array<VEX5_PORT_t>({(VEX5_PORT_t)1, (VEX5_PORT_t)2, (VEX5_PORT_t)3, (VEX5_PORT_t)4}));
+    
+    if(e) {
+        Serial.print(e.msg.c_str());
+    }
+    
+    Serial.println();
+    Serial.println(motorInterfaceAngularSpeedRange.lowest);
+    Serial.println(motorInterfaceAngularSpeedRange.highest);
+    
+    delay(5000);
     
 }
 
@@ -43,10 +53,11 @@ void go(double angle, double speed) {
     Serial.print("; 4: ");
     Serial.println(speeds()[3]);
     
-    auto err = plat.setSpeeds(speeds);
+    auto err = plat.setSpeeds(speeds());
     
     if(err) {
         Serial.println(("Ooops, something went wrong in applying speeds to motors for linear movement of the platform: " + err.msg).c_str());
+        Serial.println(err.msg.c_str());
     }
 }
 
@@ -56,12 +67,12 @@ void move(double angle, double speed, ull_t delayMs) {
 }
 
 double speed = 1000;
-ull_t sectionTime = 2000;
+ull_t sectionTime = 3000;
 
 void loop() {
     move(0, speed, sectionTime);
-    // move(90, speed, sectionTime);
-    // move(180, speed, sectionTime);
-    // move(-90, speed, sectionTime);
+    move(90, speed, sectionTime);
+    move(180, speed, sectionTime);
+    move(-90, speed, sectionTime);
     // plat.setSpeeds(platform::PlatformMotorSpeeds({motor::Speed(3000), motor::Speed(3000), motor::Speed(3000), motor::Speed(3000)}));
 }
