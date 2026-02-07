2e66a5d5599047a46d0782e911eb649bf061d9e2
allivka <allivka1920@gmail.com>
2026-02-07 13:08:47 +0300

    did some stuff

--- desktop_app/main.go
+++ desktop_app/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"image/color"
 	"time"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Update Time")
+	w := a.NewWindow("Hello")
 
-	message := widget.NewLabel("Welcome")
-	button := widget.NewButton("Update", func() {
-		formatted := time.Now().Format("Time: 03:04:05")
-		message.SetText(formatted)
-	})
+	output := canvas.NewText(time.Now().Format(time.TimeOnly), color.NRGBA{G: 0xff, A: 0xff})
+	output.TextStyle.Monospace = true
+	output.TextSize = 32
+	w.SetContent(output)
 
-	w.SetContent(container.NewVBox(message, button))
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		for range ticker.C {
+			fyne.Do(func() {
+				output.Text = time.Now().Format(time.TimeOnly)
+				output.Refresh()
+			})
+		}
+	}()
 	w.ShowAndRun()
 }
