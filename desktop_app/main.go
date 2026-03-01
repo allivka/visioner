cefa37db6580b5e582828e8e44fbe4ff91c85948
allivka <allivka1920@gmail.com>
2026-03-01 13:22:16 +0300

    little update

--- desktop_app/main.go
+++ desktop_app/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"image/color"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
+
+	// "fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 
 	"fyne.io/fyne/v2/app"
 )
+
+func getAngle() float64 {
+	
+	return 0
+}
 
 func main() {
 	
@@ -24,22 +32,44 @@ func main() {
 	input := widget.NewEntry()
 	
 	robotImage := canvas.NewImageFromFile("robot.png")
-	robotImage.SetMinSize(fyne.NewSize(500, 500))
+	robotImage.FillMode = canvas.ImageFillOriginal
+	
+	angleText := canvas.NewText("Angle: 0", color.Black)
+	angleText.TextSize = 32
+	
+	window := application.NewWindow("Visioner controller")
+	
+	window.SetContent(container.NewStack(
+		canvas.NewRectangle(color.White),
+		container.NewCenter(
+			container.NewVBox(
+				container.NewCenter(angleText),
+				robotImage,
+			),
+		),
+	))
 	
 	content := container.NewVBox(label, input, widget.NewButton("Submit", func() {
 		fmt.Println(input.Text)
 		loginWindow.Close()
 		
-		window := application.NewWindow("Visioner controller")
-		window.SetContent(container.New(layout.NewCenterLayout(),
-			robotImage,
-		))
 		window.Show()
 	}))
-
+	
 	loginWindow.SetContent(content)
 	
 	loginWindow.Show()
+	
+	go func(){
+		ticker := time.NewTicker(time.Second / 10)
+		
+		for range ticker.C {
+			fyne.Do(func() {
+				angleText.Text = "Angle: " + fmt.Sprint(getAngle())
+				angleText.Refresh()
+			})
+		}
+	}()
 	
 	application.Run()
 }
