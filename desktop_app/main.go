7676f7792ea8e9f1e6bb27e867ba34454b3c2e80
allivka <allivka1920@gmail.com>
2026-03-07 13:58:48 +0300

    added arrow rotation

--- desktop_app/main.go
+++ desktop_app/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/binary"
-	// "fmt"
+	"fmt"
 	"image/color"
 	"io"
 	"log/slog"
@@ -40,6 +40,14 @@ func(v Visioner) getAngle() (float32, error) {
 	return math.Float32frombits(binary.LittleEndian.Uint32(buffer)), nil
 }
 
+func rotateLine(origin *fyne.Position, final *fyne.Position, angle float32) {
+	vec := fyne.NewPos(final.X - origin.X, final.Y - origin.Y)
+	
+	sin, cos := math.Sincos(float64(angle / 180 * math.Pi))
+	
+	*final = fyne.NewPos(float32(cos * float64(vec.X) - sin * float64(vec.Y)) + origin.X, float32(sin * float64(vec.X) + cos * float64(vec.Y)) + origin.Y)
+}
+
 type Arrow struct {
 	l1, l2, l3, l4, l5, l6 *canvas.Line
 	
@@ -75,43 +83,52 @@ func(a *Arrow) Construct() *Arrow {
 	return a
 }
 
-func (a Arrow) render(pos fyne.Position, angle float32, scale float32) {
+func (a Arrow) render(pos fyne.Position, origin fyne.Position, angle float32, scale float32) {
 	a.pos1 = fyne.NewPos(pos.X - 20 * scale, pos.Y)
 	a.pos2 = fyne.NewPos(pos.X + 20 * scale, pos.Y)
 	a.pos3 = fyne.NewPos(pos.X - 20 * scale, pos.Y - 100 * scale)
 	a.pos4 = fyne.NewPos(pos.X + 20 * scale, pos.Y - 100 * scale)
 	a.pos5 = fyne.NewPos(pos.X, pos.Y - 125 * scale)
 	
+	rotateLine(&origin, &a.pos1, angle)
+	rotateLine(&origin, &a.pos2, angle)
+	rotateLine(&origin, &a.pos3, angle)
+	rotateLine(&origin, &a.pos4, angle)
+	rotateLine(&origin, &a.pos5, angle)
+	
 	a.l1.Position1 = a.pos1
 	a.l1.Position2 = a.pos2
-	a.l1.Refresh()
 	
 	a.l2.Position1 = a.pos1
 	a.l2.Position2 = a.pos3
-	a.l2.Refresh()
 	
 	a.l3.Position1 = a.pos2
 	a.l3.Position2 = a.pos4
-	a.l3.Refresh()
 	
 	a.l4.Position1 = a.pos3
 	a.l4.Position2 = a.pos4
-	a.l4.Refresh()
 	
 	a.l5.Position1 = a.pos3
 	a.l5.Position2 = a.pos5
-	a.l5.Refresh()
 	
 	a.l6.Position1 = a.pos4
 	a.l6.Position2 = a.pos5
+		
+	a.l1.Refresh()
+	a.l2.Refresh()
+	a.l3.Refresh()
+	a.l4.Refresh()
+	a.l5.Refresh()
 	a.l6.Refresh()
+	
 }
-
-// func (a *Arrow) draw()
 
 func main() {
 	
-	var visioner Visioner
+	var (
+		visioner Visioner
+		angleChan chan float32 = make(chan float32)
+	)
 	
 	application := app.New()
 	application.SetIcon(canvas.NewImageFromFile("Icon.png").Resource)
@@ -151,7 +168,6 @@ func main() {
 	
 	content := container.NewVBox(label, input, widget.NewButton("Submit", func() {
 		
-		
 		visioner.url = input.Text
 		_, err := visioner.getAngle()
 		
@@ -166,8 +182,15 @@ func main() {
 			ticker := time.NewTicker(time.Second / 60)
 			s := fyne.NewSize(0, 0)
 			pos := fyne.NewPos(0, 0)
+			var angle float32
+			
+			go func(){
+				for angle = range angleChan {}
+			}()
+			
 			
 			for range ticker.C {
+				
 				fyne.Do(func() {
 					s = fyne.NewSize(robotContainer.Size().Width - 100, window.Canvas().Size().Height - label.Size().Height - 100)
 					circle.Resize(fyne.NewSize(min(s.Height, s.Width), min(s.Height, s.Width)))
@@ -175,7 +198,42 @@ func main() {
 					circle.Move(pos)
 					circle.Refresh()
 					
-					arrow.render(fyne.NewPos(pos.X + circle.Size().Width / 2, pos.Y + 100 * circle.Size().Height / 100 / 3), 0, circle.Size().Height / 100 / 3)
+					arrow.render(
+						fyne.NewPos(pos.X + circle.Size().Width / 2, pos.Y + 100 * circle.Size().Height / 100 / 3),
+						fyne.NewPos(pos.X + circle.Size().Width / 2, pos.Y + circle.Size().Height / 2),
+						angle,
+						circle.Size().Height / 100 / 3,
+					)
+				})
+			}
+		}()
+		
+		time.Sleep(time.Microsecond * 100)
+		
+		go func(){
+			ticker := time.NewTicker(time.Second / 10)
+			var (
+				err error
+				angle float32
+			)
+			
+			_ = err
+			
+			for range ticker.C {
+				fyne.Do(func() {
+					// angle, err = visioner.getAngle()
+					
+					// if err != nil {
+					// 	slog.Warn(err.Error())
+					// 	return
+					// }
+					
+					angle++
+					
+					angleChan <- angle
+					
+					angleText.Text = "Angle: " + fmt.Sprint(angle) + "°"
+					angleText.Refresh()
 				})
 			}
 		}()
@@ -186,17 +244,6 @@ func main() {
 	loginWindow.SetContent(content)
 	
 	loginWindow.Show()
-	
-	go func(){
-		ticker := time.NewTicker(time.Second / 10)
-		
-		for range ticker.C {
-			fyne.Do(func() {
-				// angleText.Text = "Angle: " + fmt.Sprint(visioner.getAngle())
-				angleText.Refresh()
-			})
-		}
-	}()
 	
 	application.Run()
 }
